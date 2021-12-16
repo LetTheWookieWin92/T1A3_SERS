355b0d111ac6af0b4e3583ce7ba2942e661a5492
Geoff Reid <geoff_reid@outlook.com>
2021-12-16 13:03:14 +1100

    Created look-up program function, which can be called within index.rb to look up a program in the programs.json

--- index.rb
+++ index.rb
@@ -11,3 +11,11 @@ require_relative './methods.rb'
 # end
 
 
+# Example code for getting a student by SID
+# result = getProgram("Science")
+
+# if result.nil? 
+#     puts "nope"
+# else
+#     puts result["permitted_classes"]
+# end
--- methods.rb
+++ methods.rb
@@ -13,3 +13,19 @@ def getStudent(input_SID)
     # Return student
     return found_student
 end 
+
+# Look up method, which accepts an SID as an input and returns the student hash
+def getProgram(program_name)
+    
+    # Read the Students.json file
+    programsJSON = File.read("./data/programs.json")
+
+    # Parse the JSON into a hash
+    programsHash = JSON.parse(programsJSON)
+
+    # Look for inputted SID amongst list of students
+    found_program = programsHash.find { |program| program["name"] == program_name }
+
+    # Return student
+    return found_program
+end 
